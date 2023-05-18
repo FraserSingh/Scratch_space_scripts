12ca7606b1741e5299cda42553896d9b54cba706
FraserSingh <fraser.singh@hotmail.com>
2023-05-19 00:42:22 +0100

    attempt at cellranger looped for all Ximerakis data

--- Project_23/External_datasets/Ximerakis/CR_counts_Ximerakis.sh
+++ Project_23/External_datasets/Ximerakis/CR_counts_Ximerakis.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
 #$ -cwd             #run in current working dir
 #$ -N ximerakis_CR_counts    #name of job
-#$ -l h_rt=15:00:00 #approximate time taken, (specify more than required for safety)
+#$ -l h_rt=30:00:00 #approximate time taken, (specify more than required for safety)
 #$ -l h_vmem=8G     #How much RAM is required
 #$ -pe sharedmem 3  #how many cores?
 #$ -e ximerakis_CR_counts.e     #where errors go
@@ -11,22 +11,21 @@
 
 #Run in Ximerakis (directory above Dumped/*fastq.gz )
 
-#Make filename to use .txt?
+#Grant access to cellranger
+export PATH=/exports/eddie/scratch/s2268606/yard/cellranger-7.1.0:$PATH #fixme
 
-#!/bin
-find ./Dumped -maxdepth 1 -name "*.fastq.gz"  -print|sort|uniq >Ximerakis_CR_files.txt
+#Make .txt to use filenames
+find ./Dumped -maxdepth 1 -name "*.fastq.gz" -print | xargs -I{} basename {} .fastq.gz | sort | uniq > Ximerakis_CR_files.txt
 
 
 #run for loop to run cellranger for each sample in Ximerakis folder (16 samples)
 
 for F in $(cat Ximerakis_CR_files.txt) ; do
 
-        FULLSTRING=$F #do the name filtering here
-        fastqc  ${FULLSTRING} -t 20  -o ./fastqc_merged
-
-cellranger count --id=  \ #needs to be FULLSTRING without the fastq.gz file extension, 
-   --fastqs= /mnt/home/user.name/yard/run_cellranger_count/pbmc_1k_v3_fastqs \
-   --sample= pbmc_1k_v3 \ #This --sample argument works off of the sample id at the beginning of the FASTQ file name so this will be the SRA sample number in the file name
-   --transcriptome= /mnt/home/user.name/yard/run_cellranger_count/refdata-gex-GRCh38-2020-A
+        FULLSTRING=$F 
+	cellranger count --id= ${FULLSTRING}_CR \ #without the fastq.gz file extension or pathname
+   	--fastqs= /exporteddie/scratch/s2268606/Project_23/External_datasets/Ximerakis/Dumped/ 
+   	--sample= ${FULLSTRING} \ #This --sample argument works off of the sample id at the beginning of the FASTQ file name so this will be the SRA sample number in the file name
+   	--transcriptome= /exports/eddie/scratch/s2268606/Project_23/Ensembl_refs/Cellranger_Mouse_genome/ 
 
 done
