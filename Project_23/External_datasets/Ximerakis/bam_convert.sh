bc0d5637f4758d6642ec08abaec690f147464f8e
FraserSingh <fraser.singh@hotmail.com>
2023-05-22 15:50:29 +0100

    changeing paths so the script operates int he same place

--- Project_23/External_datasets/Ximerakis/bam_convert.sh
+++ Project_23/External_datasets/Ximerakis/bam_convert.sh
@@ -20,10 +20,9 @@ export PATH=/exports/eddie/scratch/s2268606/yard/cellranger-7.1.0:$PATH
 #To be run in Ximerakis folder
 #make directories for results
 
-find ./bam_testing_to_be_removed -maxdepth 1 -name "*.bam.1" -print | xargs -I{} basename {} .bam.1 | sort | uniq > Ximerakis_BAM_names.txt
+find ./ -maxdepth 1 -name "*.bam.1" -print | xargs -I{} basename {} .bam.1 | sort | uniq > Ximerakis_BAM_names.txt
 
 for B in $(cat Ximerakis_BAM_names.txt) ; do
 	BAMNAME=$B
-	mkdir ${BAMNAME}
-	cellranger bamtofastq  ./bam_testing_to_be_removed/${BAMNAME}.bam.1 ./${BAMNAME}
+	cellranger bamtofastq  ./${BAMNAME}.bam.1 ./${BAMNAME}
 done
