34abd45ff888f7eb3947bbc9813aadd211ba3db7
FraserSingh <fraser.singh@hotmail.com>
2023-05-22 15:44:40 +0100

    automating bam conversion

--- Project_23/External_datasets/Ximerakis/bam_convert.sh
+++ Project_23/External_datasets/Ximerakis/bam_convert.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/sh
 #$ -cwd             #run in current working dir
-#$ -N bam2fastq    #name of job
-#$ -l h_rt=05:00:00 #approximate time taken, (specify more than required for safety)
+#$ -N bam2fastqFull    #name of job
+#$ -l h_rt=08:00:00 #approximate time taken, (specify more than required for safety)
 #$ -l h_vmem=16G     #How much RAM is required
-#$ -pe sharedmem 3  #how many cores?
-#$ -e bam2fastq.e     #where errors go
+#$ -pe sharedmem 4  #how many cores?
+#$ -e bam2fastqFull.e     #where errors go
 #$ -M S2268606@ed.ac.uk    #contact info
 #$ -m beas 
 
@@ -17,4 +17,13 @@ source activate project_test
 
 export PATH=/exports/eddie/scratch/s2268606/yard/cellranger-7.1.0:$PATH
 
-cellranger bamtofastq  ./bam_testing_to_be_removed/OX2X.bam.1 ./bam_testing_to_be_removed/SRR8895023
+#To be run in Ximerakis folder
+#make directories for results
+
+find ./bam_testing_to_be_removed -maxdepth 1 -name "*.bam.1" -print | xargs -I{} basename {} .bam.1 | sort | uniq > Ximerakis_BAM_names.txt
+
+for B in $(cat Ximerakis_BAM_names.txt) ; do
+	BAMNAME=$B
+	mkdir ${BAMNAME}
+	cellranger bamtofastq  ./bam_testing_to_be_removed/${BAMNAME}.bam.1 ./${BAMNAME}
+done
