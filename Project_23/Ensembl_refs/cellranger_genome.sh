153f0d043e9dce62f13094e0535d4be95e64ff04
FraserSingh <fraser.singh@hotmail.com>
2023-06-14 18:31:37 +0100

    started script on msc6 for soupx, updated Ximerakis

--- Project_23/Ensembl_refs/cellranger_genome.sh
+++ Project_23/Ensembl_refs/cellranger_genome.sh
@@ -11,7 +11,7 @@ export PATH=/exports/eddie/scratch/s2268606/yard/cellranger-7.1.0:$PATH #fixme
 
 
 
-#wget https://ftp.ensembl.org/pub/release-109/fasta/mus_musculus/dna/Mus_musculus.GRCm39.dna.primary_assembly.fa.gz
-#wget https://ftp.ensembl.org/pub/release-109/gtf/mus_musculus/Mus_musculus.GRCm39.109.gtf.gz
+wget https://ftp.ensembl.org/pub/release-109/fasta/mus_musculus/dna/Mus_musculus.GRCm39.dna.primary_assembly.fa.gz
+wget https://ftp.ensembl.org/pub/release-109/gtf/mus_musculus/Mus_musculus.GRCm39.109.gtf.gz
 
 cellranger mkref --genome Cellranger_Mouse_genome --fasta ./Mus_musculus.GRCm39.dna.primary_assembly.fa --genes Mus_musculus.GRCm39.109.gtf
